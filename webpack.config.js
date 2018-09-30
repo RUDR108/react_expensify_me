07d73fffd6561ee8a5c5632b8f4168cfb8334547
RUDR108 <34435910+akshit777@users.noreply.github.com>
2018-09-30 18:47:55 +0530

    setup production webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
-//entry -> output
+// entry->output
 
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path=require('path');
 
-
-module.exports={
+module.exports=(env)=>{
+    const isProduction=env==='production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+return{
     entry:'./src/app.js',
     output:{
         path:path.join(__dirname,'public'),
@@ -16,21 +19,32 @@ module.exports={
             exclude:/node_modules/
         },{
             test:/\.s?css$/,
-            use:[
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-
-            ]
+            use:CSSExtract.extract({
+                use:[
+                    {loader:'css-loader',
+                options:{
+                    sourceMap:true
+                }},
+                    {loader:'sass-loader',
+                options:{
+                    sourceMap:true
+                }}
+                ]
+            })
         }]
     },
-    devtool:"cheap-module-eval-source-map",
-     //(source-map)->to go to debug in src file rather than bundle file.
+    plugins:[
+        CSSExtract
+    ],
+    devtool:isProduction?'source-map':'inline-source-map',
+    
     devServer:{
 
         contentBase:path.join(__dirname,'public'),
         historyApiFallback:true
     }
 };
+};
+
 
 
